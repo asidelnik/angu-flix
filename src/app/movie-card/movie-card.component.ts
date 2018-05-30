ed5d8817f4ad3172fa747538dd0a676bf1c0fbd2
Amos <asidelnik@gmail.com>
2018-05-30 20:45:45 +0300

    movie-card-comp - added and fixed viewing cards. Fix purchase calling to service across parent

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Movie } from '../movie';
 
 @Component({
   selector: 'app-movie-card',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-card.component.scss']
 })
 export class MovieCardComponent implements OnInit {
-
+    @Input() movie: Movie;
+    // @Input('master') masterName: string;
   constructor() { }
 
   ngOnInit() {
+    //   console.log(this.movie);
   }
-
+  
+//   addMoviePar(movie) {
+//     console.log("movies:");      
+//     this.moviesService.addMovieServ(movie);
+//  }
 }
